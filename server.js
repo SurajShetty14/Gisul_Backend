cb40320f55ef5635bea4a70a1c4111d6e27acded
Suraj Shetty <surajshetty2804@gmail.com>
2025-07-15 18:33:31 +0530

    Signup-login-logout-profilepage-cors

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://www.snibo.co', // <-- your Webflow (or custom domain) site
+  credentials: true
+}));
 app.use(express.json());
 
 // Session middleware
