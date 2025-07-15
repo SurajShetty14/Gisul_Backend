e4a221959106e60ccd10ed20e2643d9be2b4636a
Suraj Shetty <surajshetty2804@gmail.com>
2025-07-15 13:40:06 +0530

    Signup-login-logout-profilepage

--- server.js
+++ server.js
@@ -194,12 +194,9 @@ app.put('/profile', async (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('✅ Connected to MongoDB Atlas'))
+  .catch((err) => console.error('❌ Connection error:', err));
 
 // Start server
 app.listen(PORT, () => {
