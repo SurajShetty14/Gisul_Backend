4baba422cf4331c4c4fb6dcc76ff7c31e2b89cb4
Suraj Shetty <surajshetty2804@gmail.com>
2025-08-07 11:05:01 +0530

    only gisul cors url

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://www.gisul.co', 'https://api.gisul.co.in'],
+  origin: ['https://gisul.co', 'https://api.gisul.co.in'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
